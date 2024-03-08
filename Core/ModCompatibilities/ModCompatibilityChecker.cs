9aaa65775e25f15a590ad1eaa912608b7d5410d0
Yum <jallygaming@gmail.com>
2024-03-08 07:46:24 +0200

    Обновление русификатора 3.1.1.23

--- Core/ModCompatibilities/ModCompatibilityChecker.cs
+++ Core/ModCompatibilities/ModCompatibilityChecker.cs
@@ -12,7 +12,7 @@ public class ModCompatibilityChecker : ModSystem
         new("StarlightRiver", new Version(1, 5, 0, 2), false),
         new("InfernumMode", new Version(2, 0, 1), true),
         new("FargowiltasSouls", new Version(1, 6, 7, 1), true),
-        new("Fargowiltas", new Version(3, 3, 1), true),
+        new("Fargowiltas", new Version(3, 3, 1, 1), true),
         new("ThoriumMod", new Version(1, 7, 1, 5), true),
     };
 
