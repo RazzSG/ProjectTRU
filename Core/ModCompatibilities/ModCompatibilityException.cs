1cdb4969cb46e10d856f2a0bac9592d36d909aac
Yum <jallygaming@gmail.com>
2024-03-11 09:35:04 +0200

    Обновление русификатора 3.1.1.25

--- Core/ModCompatibilities/ModCompatibilityException.cs
+++ Core/ModCompatibilities/ModCompatibilityException.cs
@@ -13,7 +13,7 @@ public class ModCompatibilityException : Exception
     {
         if (currentModVersion > expectedModVersion)
         {
-            return $"У вас установлен [c/FFF783:{CalamityRuTranslate.Instance.DisplayName}], который не поддерижвает версию [c/FFF783:{modName}] [c/00FF09:{currentModVersion}]. Для корректной работы необходимо дождаться обновления [c/FFF783:{CalamityRuTranslate.Instance.DisplayName}], которое будет поддерживать версию [c/00FF09:{currentModVersion}].\n";
+            return $"У вас установлен [c/FFF783:{CalamityRuTranslate.Instance.DisplayName}], который не поддерживает версию [c/FFF783:{modName}] [c/00FF09:{currentModVersion}]. Для корректной работы необходимо дождаться обновления [c/FFF783:{CalamityRuTranslate.Instance.DisplayName}], которое будет поддерживать версию [c/00FF09:{currentModVersion}].\n";
         }
 
         return $"У вас установлена несовместимая версия [c/FFF783:{modName}]. Ваша текущая версия: [c/FF0000:{currentModVersion}]. Для корректной работы русификатора необходимо зайти в браузер модов и обновить [c/FFF783:{modName}] до версии [c/00FF09:{expectedModVersion}].\n";
