70c6e09e4c831a25176c8174c9a9fbf6fae94015
Yum <jallygaming@gmail.com>
2024-05-18 22:30:27 +0300

    Обновление русификатора 3.1.1.45

--- Mods/CalamityMod/CalamityModSystem.cs
+++ Mods/CalamityMod/CalamityModSystem.cs
@@ -3,13 +3,26 @@ using CalamityMod.Items.Placeables.Furniture.DevPaintings;
 using CalamityMod.UI.DraedonSummoning;
 using CalamityRuTranslate.Common;
 using CalamityRuTranslate.Common.Utilities;
+using CalamityRuTranslate.Core.Config;
+using ReLogic.Content;
 using ReLogic.Graphics;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace CalamityRuTranslate.Mods.CalamityMod;
 
 public class CalamityModSystem : ModSystem
 {
+    private Asset<DynamicSpriteFont> CodebreakerFont { get; set; }
+
+    public override void Load()
+    {
+        if (Main.dedServ)
+            return;
+        
+        CodebreakerFont = Mod.Assets.Request<DynamicSpriteFont>("Assets/Fonts/Item_Stack");
+    }
+
     public override bool IsLoadingEnabled(Mod mod)
     {
         return ModInstances.Calamity != null && TranslationHelper.IsRussianLanguage;
@@ -20,8 +33,11 @@ public class CalamityModSystem : ModSystem
         ThankYouPainting.devList.RemoveAt(0);
         ThankYouPainting.devList.Insert(0, "Fabsol, основатель и владелец мода");
         
+        DynamicSpriteFont replaceFont = TRuConfig.Instance.NewRussianTerrariaFont
+            ? Mod.Assets.Request<DynamicSpriteFont>("Assets/Fonts/Item_Stack").Value
+            : CodebreakerFont.Value;
+        
         PropertyInfo dialogueFont = typeof(CodebreakerUI).GetCachedProperty("DialogFont");
-        DynamicSpriteFont replaceFont = CalamityRuTranslate.Instance.Assets.Request<DynamicSpriteFont>("Assets/Fonts/Item_Stack").Value;
         dialogueFont.SetValue(null, replaceFont);
     }
 }
---
+++ Mods/Vanilla/MonoMod/SoundEnginePatch.cs
@@ -0,0 +1,40 @@
+﻿// using CalamityRuTranslate.Common;
+// using CalamityRuTranslate.Common.Utilities;
+// using CalamityRuTranslate.Mods.StarsAbove;
+// using Microsoft.Xna.Framework;  
+// using ReLogic.Utilities;
+// using StarsAbove.Systems;
+// using Terraria;
+// using Terraria.Audio;
+// using Terraria.ModLoader;
+//
+// namespace CalamityRuTranslate.Mods.Vanilla.MonoMod;
+//
+// public class SoundEnginePatch : ILoadable
+// {
+//     public bool IsLoadingEnabled(Mod mod)
+//     {
+//         return ModInstances.StarsAbove != null && TranslationHelper.IsRussianLanguage;
+//     }
+//
+//     public void Load(Mod mod)
+//     {
+//         On_SoundEngine.PlaySound_refSoundStyle_Nullable1_SoundUpdateCallback += On_SoundEngineOnPlaySoundRefSoundStyleNullable1SoundUpdateCallback;
+//     }
+//
+//     public void Unload()
+//     {
+//         On_SoundEngine.PlaySound_refSoundStyle_Nullable1_SoundUpdateCallback -= On_SoundEngineOnPlaySoundRefSoundStyleNullable1SoundUpdateCallback;
+//     }
+//     
+//     private SlotId On_SoundEngineOnPlaySoundRefSoundStyleNullable1SoundUpdateCallback(On_SoundEngine.orig_PlaySound_refSoundStyle_Nullable1_SoundUpdateCallback orig, ref SoundStyle style, Vector2? position, SoundUpdateCallback updatecallback)
+//     {
+//         StarsAboveAudio sounds = new StarsAboveAudio();
+//         if (style == sounds.AS1)
+//         {
+//             style = StarsAboveSounds.AS1;
+//         }
+//
+//         return  orig.Invoke(ref style, position, updatecallback);
+//     }
+// }
