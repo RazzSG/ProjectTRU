56223ce511f88a06651d3b5927045101b0004a9e
Yum <jallygaming@gmail.com>
2024-07-14 09:12:52 +0300

    Обновление русификатора 3.2.1.4

--- Mods/Vanilla/MonoMod/DrawPatch.cs
+++ Mods/Vanilla/MonoMod/DrawPatch.cs
@@ -82,3 +82,46 @@ public class DrawPatch : OnPatcher
         orig.Invoke(self, texture, destinationRectangle, color);
     }
 }
+
+public class Draw2Patch : OnPatcher
+{
+    private const string PATH = "Assets/Sprites/StarsAbove";
+    
+    private Dictionary<string, string> _textures = new()
+    {
+        { "UI/CelestialCartography/LocationNames/WarriorOfLight", $"{PATH}/WarriorOfLight" },
+        { "UI/CelestialCartography/LocationNames/FirstStarfarer", $"{PATH}/FirstStarfarer" },
+        { "UI/CelestialCartography/LocationNames/Vagrant", $"{PATH}/Vagrant" },
+        { "UI/CelestialCartography/LocationNames/Thespian", $"{PATH}/Thespian" },
+        { "UI/CelestialCartography/LocationNames/Dioskouroi", $"{PATH}/Dioskouroi" },
+        { "UI/CelestialCartography/LocationNames/Penthesilea", $"{PATH}/Penthesilea" },
+        { "UI/CelestialCartography/LocationNames/Nalhaun", $"{PATH}/Nalhaun" },
+        { "UI/CelestialCartography/LocationNames/Arbitration", $"{PATH}/Arbitration" },
+        { "UI/CelestialCartography/LocationNames/Starfarers", $"{PATH}/Starfarers" },
+    };
+    
+    private delegate void DrawDelegate(SpriteBatch self, Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth);
+    
+    public override bool AutoLoad => TranslationHelper.IsRussianLanguage;
+
+    public override MethodInfo ModifiedMethod => typeof(SpriteBatch).GetCachedMethod("Draw", [typeof(Texture2D), typeof(Vector2), typeof(Rectangle?)
+    , typeof(Color), typeof(float), typeof(Vector2), typeof(float), typeof(SpriteEffects), typeof(float)]);
+
+    public override Delegate Delegate => Translate;
+
+    private void Translate(DrawDelegate orig, SpriteBatch self, Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth)
+    {
+        if (ModInstances.StarsAbove != null && TRuConfig.Instance.StarsAboveLocalization)
+        {
+            foreach (KeyValuePair<string, string> path in _textures)
+            {
+                if (texture == ModInstances.StarsAbove.Assets.Request<Texture2D>(path.Key).Value)
+                {
+                    texture = CalamityRuTranslate.Instance.Assets.Request<Texture2D>(path.Value).Value;
+                }
+            }
+        }
+
+        orig.Invoke(self, texture, position, sourceRectangle, color, rotation, origin, scale, effects, layerDepth);
+    }
+}
