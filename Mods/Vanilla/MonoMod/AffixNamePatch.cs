42f2ef759737a7f18f6b55b80ccfd9320e0b0afd
Yum <jallygaming@gmail.com>
2024-08-31 13:36:34 +0300

    Обновление русификатора 3.2.1.20

--- Mods/Vanilla/MonoMod/AffixNamePatch.cs
+++ Mods/Vanilla/MonoMod/AffixNamePatch.cs
@@ -27,21 +27,15 @@ public class AffixName : ILoadable
 
     private string ItemOnAffixName(On_Item.orig_AffixName orig, Item self)
     {
-        if (self.prefix < 0 && self.prefix >= Lang.prefix.Length)
-            return self.Name;
-
-        string prefix = Lang.prefix[self.prefix].Value;
+        string result = orig.Invoke(self);
         PrefixOverhaul prefixOverhaul = new();
-        if (prefix == string.Empty)
-            return self.Name;
-
         foreach (var t in prefixOverhaul.Prefixes)
         {
-            if (t[0] == prefix)
+            if (t[0] == Lang.prefix[self.prefix].Value)
                 return prefixOverhaul.GetGenderedPrefix(t, self.type) + " " + (self.Name.Contains('.') ? self.Name : self.Name.ToLower());
         }
 
-        return prefix + " " + self.Name;
+        return result;
     }
 }
 
@@ -64,27 +58,36 @@ public class AffixNameWithCalamity : ILoadable
 
     private string ItemOnAffixName(On_Item.orig_AffixName orig, Item self)
     {
-        string calamityEnchantment = string.Empty, goblinPrefix = string.Empty;
+        if (self.prefix < 0 || self.prefix >= Lang.prefix.Length)
+            return self.Name;
+
         PrefixOverhaul prefixOverhaul = new();
-        
-        foreach (string[] t in prefixOverhaul.Prefixes)
+        string goblinPrefix = Lang.prefix[self.prefix].Value;
+        string calamityEnchantment = string.Empty;
+
+        foreach (var t in prefixOverhaul.Prefixes)
         {
             if (!self.IsAir && self.TryGetGlobalItem(out CalamityGlobalItem calamityGlobalItem) && calamityGlobalItem.AppliedEnchantment.HasValue)
             {
                 if (t[0] == calamityGlobalItem.AppliedEnchantment?.Name.ToString())
-                    calamityEnchantment = prefixOverhaul.GetGenderedPrefix(t, self.type) + " ";
-
-                if (calamityEnchantment != string.Empty)
-                    goblinPrefix = goblinPrefix.ToLower();
+                    calamityEnchantment = prefixOverhaul.GetGenderedPrefix(t, self.type);
             }
 
-            if (t[0] == Lang.prefix[self.prefix].Value)
-                goblinPrefix = prefixOverhaul.GetGenderedPrefix(t, self.type) + " ";
+            if (t[0] == goblinPrefix)
+                goblinPrefix = prefixOverhaul.GetGenderedPrefix(t, self.type);
         }
 
-        if (goblinPrefix == string.Empty && calamityEnchantment == string.Empty)
-            return self.Name;
+        string formattedName = self.Name.Contains('.') ? self.Name : self.Name.ToLower();
         
-        return calamityEnchantment + goblinPrefix + (self.Name.Contains('.') ? self.Name : self.Name.ToLower());
+        if (calamityEnchantment != string.Empty && goblinPrefix == string.Empty)
+            return calamityEnchantment + " " + formattedName;
+
+        if (goblinPrefix != string.Empty && calamityEnchantment == string.Empty)
+            return goblinPrefix + " " + formattedName;
+
+        if (calamityEnchantment != string.Empty && goblinPrefix != string.Empty)
+            return calamityEnchantment + " " + goblinPrefix.ToLower() + " " + formattedName;
+
+        return self.Name;
     }
 }
