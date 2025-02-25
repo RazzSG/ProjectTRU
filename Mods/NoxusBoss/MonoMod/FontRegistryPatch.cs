0d46fe68c742210e6264073f25f62abf3f36a4fd
Yum <jallygaming@gmail.com>
2025-02-25 14:03:57 +0200

    Обновление русификатора 3.2.1.66

--- Mods/NoxusBoss/MonoMod/FontRegistryPatch.cs
+++ Mods/NoxusBoss/MonoMod/FontRegistryPatch.cs
@@ -41,3 +41,51 @@ public class NamelessDeityTextPatch : OnPatcher
         return CalamityRuTranslate.Instance.Assets.Request<DynamicSpriteFont>("Assets/Fonts/FairyMuffinRoundPop", AssetRequestMode.ImmediateLoad).Value;
     }
 }
+
+public class SolynTextPatch : OnPatcher
+{
+    private delegate DynamicSpriteFont SolynTextDelegate(FontRegistry self);
+
+    public override bool AutoLoad => ModInstances.NoxusBoss != null && TRuConfig.Instance.NoxusBossLocalization && TranslationHelper.IsRussianLanguage;
+
+    public override MethodInfo ModifiedMethod => typeof(FontRegistry).GetCachedMethod("get_SolynText");
+
+    public override Delegate Delegate => Translation;
+    
+    private DynamicSpriteFont Translation(SolynTextDelegate orig, FontRegistry self)
+    {
+        return CalamityRuTranslate.Instance.Assets.Request<DynamicSpriteFont>("Assets/Fonts/Death_Text", AssetRequestMode.ImmediateLoad).Value;
+    }
+}
+
+// public class SolynTextItalicsPatch : OnPatcher
+// {
+//     private delegate DynamicSpriteFont SolynTextItalicsDelegate(FontRegistry self);
+//
+//     public override bool AutoLoad => ModInstances.NoxusBoss != null && TRuConfig.Instance.NoxusBossLocalization && TranslationHelper.IsRussianLanguage;
+//
+//     public override MethodInfo ModifiedMethod => typeof(FontRegistry).GetCachedMethod("get_SolynTextItalics");
+//
+//     public override Delegate Delegate => Translation;
+//     
+//     private DynamicSpriteFont Translation(SolynTextItalicsDelegate orig, FontRegistry self)
+//     {
+//         return CalamityRuTranslate.Instance.Assets.Request<DynamicSpriteFont>("Assets/Fonts/SolynTextItalics", AssetRequestMode.ImmediateLoad).Value;
+//     }
+// }
+
+public class SolynFightDialoguePatch : OnPatcher
+{
+    private delegate DynamicSpriteFont SolynFightDialogueDelegate(FontRegistry self);
+
+    public override bool AutoLoad => ModInstances.NoxusBoss != null && TRuConfig.Instance.NoxusBossLocalization && TranslationHelper.IsRussianLanguage;
+
+    public override MethodInfo ModifiedMethod => typeof(FontRegistry).GetCachedMethod("get_SolynFightDialogue");
+
+    public override Delegate Delegate => Translation;
+    
+    private DynamicSpriteFont Translation(SolynFightDialogueDelegate orig, FontRegistry self)
+    {
+        return CalamityRuTranslate.Instance.Assets.Request<DynamicSpriteFont>("Assets/Fonts/SolynFightDialogue", AssetRequestMode.ImmediateLoad).Value;
+    }
+}
