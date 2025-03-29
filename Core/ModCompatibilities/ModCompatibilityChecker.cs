605b84c1062683d84ffe35406426509c3a633239
Yum <jallygaming@gmail.com>
2025-03-29 07:35:12 +0200

    Обновление русификатора 3.2.1.82

--- Core/ModCompatibilities/ModCompatibilityChecker.cs
+++ Core/ModCompatibilities/ModCompatibilityChecker.cs
@@ -19,11 +19,11 @@ public class ModCompatibilityChecker : ModSystem
         new("CalamityMod", new Version(2, 0, 4, 6), TRuConfig.Instance.CalamityModLocalization),
         new("CatalystMod", new Version(1, 1, 2, 3), TRuConfig.Instance.CatalystLocalization),
         new("Fargowiltas", new Version(3, 3, 6, 1), TRuConfig.Instance.FargowiltasLocalization),
-        new("FargowiltasSouls", new Version(1, 7, 2, 4), TRuConfig.Instance.FargowiltasSoulsLocalization),
+        new("FargowiltasSouls", new Version(1, 7, 2, 5), TRuConfig.Instance.FargowiltasSoulsLocalization),
         new("InfernumMode", new Version(2, 0, 1, 17), TRuConfig.Instance.InfernumModeLocalization),
         new("NoxusBoss", new Version(1, 2, 19), TRuConfig.Instance.NoxusBossLocalization),
         new("Redemption", new Version(0, 8, 0, 4072), TRuConfig.Instance.RedemptionLocalization),
-        new("SpiritReforged", new Version(0, 1, 0, 5), TRuConfig.Instance.SpiritReforgedLocalization),
+        new("SpiritReforged", new Version(0, 1, 0, 6), TRuConfig.Instance.SpiritReforgedLocalization),
         new("StarsAbove", new Version(2, 1, 3, 4), TRuConfig.Instance.StarsAboveLocalization),
         new("ThoriumMod", new Version(1, 7, 2, 3), TRuConfig.Instance.ThoriumModLocalization)
     ];
