32c2cc19a817263f287cac2a6946c4c135f72fc7
Yum <jallygaming@gmail.com>
2025-11-08 08:21:31 +0200

    Обновление русификатора 3.2.1.111

--- Core/ModCompatibilities/ModCompatibilityChecker.cs
+++ Core/ModCompatibilities/ModCompatibilityChecker.cs
@@ -19,13 +19,13 @@ public class ModCompatibilityChecker : ModSystem
         new("CalamityMod", new Version(2, 0, 6, 2), TRuConfig.Instance.CalamityModLocalization),
         new("CatalystMod", new Version(1, 1, 2, 3), TRuConfig.Instance.CatalystLocalization),
         new("Fargowiltas", new Version(3, 3, 6, 5), TRuConfig.Instance.FargowiltasLocalization),
-        new("FargowiltasSouls", new Version(1, 7, 3, 4), TRuConfig.Instance.FargowiltasSoulsLocalization),
+        new("FargowiltasSouls", new Version(1, 7, 3, 5), TRuConfig.Instance.FargowiltasSoulsLocalization),
         new("InfernumMode", new Version(2, 0, 1, 20), TRuConfig.Instance.InfernumModeLocalization),
         new("NoxusBoss", new Version(1, 2, 25), TRuConfig.Instance.NoxusBossLocalization),
         new("Redemption", new Version(0, 8, 0, 4084), TRuConfig.Instance.RedemptionLocalization),
-        new("SpiritReforged", new Version(0, 1, 2, 2), TRuConfig.Instance.SpiritReforgedLocalization),
-        new("StarsAbove", new Version(2, 1, 7, 3), TRuConfig.Instance.StarsAboveLocalization),
-        new("ThoriumMod", new Version(1, 7, 2, 4), TRuConfig.Instance.ThoriumModLocalization)
+        new("SpiritReforged", new Version(0, 1, 2, 4), TRuConfig.Instance.SpiritReforgedLocalization),
+        new("StarsAbove", new Version(2, 1, 7, 5), TRuConfig.Instance.StarsAboveLocalization),
+        new("ThoriumMod", new Version(1, 7, 2, 5), TRuConfig.Instance.ThoriumModLocalization)
     ];
 
     public override bool IsLoadingEnabled(Mod mod)
